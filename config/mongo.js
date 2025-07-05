305dbc96620a8fbf4fa025b3fbb226b37de93f64
Rahul Rewar <rrewar122@gmail.com>
2025-07-05 08:56:48 +0530

    mongo file update

--- config/mongo.js
+++ config/mongo.js
@@ -1,11 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); 
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not defined in environment variables");
+    }
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log("✅ database connected successfully");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
