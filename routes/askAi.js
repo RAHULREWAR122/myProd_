2070ec0ffce79fc26732335431bda02346ba1e97
Rahul Rewar <rrewar122@gmail.com>
2025-07-05 09:18:46 +0530

    ai route update

--- routes/askAi.js
+++ routes/askAi.js
@@ -3,6 +3,6 @@ const router  = express.Router();
 import verifyToken from "../middleware/verifyAuth.js";
 import { askAssistant } from '../controllers/aiAssestant.js';
 
-router.post("/assistant", verifyToken, askAssistant);
+router.post("/assistant", askAssistant);
 
 export default router;
